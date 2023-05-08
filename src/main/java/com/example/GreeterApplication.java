478fe3fbee9306d8b6fdc6fac1026620b3e3c0be
Khaled Yakdan <yakdan@code-intelligence.com>
2023-05-08 17:31:28 +0200

    WIP

--- src/main/java/com/example/GreeterApplication.java
+++ src/main/java/com/example/GreeterApplication.java
@@ -1,19 +1,3 @@
-/*
- * Copyright 2023 Code Intelligence GmbH
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example;
 
 
@@ -31,8 +15,9 @@ import java.sql.SQLException;
 @SpringBootApplication
 @RestController
 class GreeterApplication {
+  
   @GetMapping("/hello")
-  public String insecureHello(@RequestParam(required = false, defaultValue = "World") String name) {
+  public String helloUser(@RequestParam(required = false, defaultValue = "World") String name) {
     if (name.startsWith("execute:")) {
       // SECURITY ALERT: vulnerable to Remote Code Execution since
       // it loads class that an attacker control.
@@ -45,7 +30,7 @@ class GreeterApplication {
   }
 
   @GetMapping("/greet")
-  public String insecureAddUser(@RequestParam(required = false, defaultValue = "World") String name){
+  public String greetUser(@RequestParam(required = false, defaultValue = "World") String name){
     if (name.startsWith("Hacker")) {
       try {
         Connection conn = getDBConnection();
